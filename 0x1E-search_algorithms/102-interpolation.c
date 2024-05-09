06d86bb2abb33c3645f1894fc8d772cd67868144
qwertyriks <romeokumatse@gmail.com>
2024-05-08 23:33:44 -0700

    betty 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -7,11 +7,11 @@
 /**
   * interpolation_search - Search for a value in a sorted array,
   * of integers, using interpolation search.
-  * 
+  *
   * @array: A pointer to the first element of the array to search.
-  * 
+  *
   * @size: The number of elements in the array.
-  * 
+  *
   * @value: The value to search for.
   *
   * Return: If the value is not present or the array is NULL, -1.
@@ -47,3 +47,4 @@ int interpolation_search(int *array, size_t size, int value)
 
 	return (-1);
 }
+
